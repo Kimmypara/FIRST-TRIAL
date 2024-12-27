8334449070808f8ee0a10f85e1c2131c25c091c5
Kimmypara <kparascandalo@gmail.com>
2024-12-27 17:31:28 +0100

    forms js

--- forms.js
+++ forms.js
@@ -5,16 +5,45 @@ function validate(){
 //Condition 
     if(firstName == ""){
 errors = errors+"<li>Please enter First Name</li>"
+document.getElementById("firstName").classList.add("inputError");
     }
+    else{
+        document.getElementById("firstName").classList.remove("inputError");
+    }
+    
+
+
+
 //Get the value of the user input and copy it into variable
     let lastName = document.getElementById("lastName").value;
 //Condition 
     if (lastName == ""){
         errors = errors + "<li>Please enter Last Name</li>";
     }
+//Condition for radio buttons
+    let age = document.getElementsByName("age");
+
+    let ageGroup = false;
+    for (i = 0; i < age.length; i++) {
+        if (age[i].checked){
+             ageGroup = true;
+        }     
+    }
+    if (!ageGroup){
+        errors = errors + "<li>Please select age group</li>";
+    }
+
+    let email = document.getElementById("email").value;
+
+    if (email == ""){
+        errors = errors + "<li>Please enter your email address</li>";
+    }
+    
+
+
 //If errors are not empty, meaning if we have errors, add them to the HTML list and show it
     if(errors != ""){
-        document.getElementById("errorList").innerHTML = error;
+        document.getElementById("errorList").innerHTML = errors;
         document.getElementById("errorList").classList.remove("hidden");
         return false;
     }
